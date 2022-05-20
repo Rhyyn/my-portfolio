c19205393bdbbfd1b305465e617ece3e64c1da91
Rhyn <rhyyn.lol@gmail.com>
2022-05-20 17:15:08 +0200

    Feat: New Project page

---
+++ pages/projects.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import homeStyles from "../styles/Home.module.css";
+import headerStyles from "../styles/Header.module.css";
+
+const projects = () => {
+  return (
+    <div className={homeStyles.pageContainer}>projects</div>
+  )
+}
+
+export default projects
