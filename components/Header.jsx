b705efd1ffd44b42d38400ec8e83179530189e02
Rhyn <rhyyn.lol@gmail.com>
2022-05-21 17:40:03 +0200

    Feat: language switch toggle

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,30 +1,20 @@
 import React from "react";
 import Image from "next/image";
 import headerStyles from "../styles/Header.module.css";
-import { useRouter } from "next/router";
-import Link from "next/dist/client/link";
-import { BiMailSend } from "react-icons/bi";
+import LanguageSwitch from "./LanguageSwitch";
 
 const Header = () => {
-    const { locale, locales, defaultLocale, asPath } = useRouter();
   return (
     <header className={headerStyles.header}>
       <div className={headerStyles.headerContainer}>
-        <Image className={headerStyles.logo} alt="logo.png" src="/logo.png" height={64} width={64}></Image>
-        <button>
-          <Link activeClassName={locale === "fr"} href={asPath} locale="fr">
-            FR
-          </Link>
-        </button>
-        <button>
-          <Link activeClassName={locale === "en"} href={asPath} locale="en">
-            EN
-          </Link>
-        </button>
-        <span className={headerStyles.contactText}>
-          Contact
-          <BiMailSend className={headerStyles.contactLogo} size={25} />
-        </span>
+        <Image
+          className={headerStyles.logo}
+          alt="logo.png"
+          src="/logo.png"
+          height={64}
+          width={64}
+        ></Image>
+        <LanguageSwitch/>
       </div>
     </header>
   );
