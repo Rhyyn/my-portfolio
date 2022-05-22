8a0e45d8caf22097dd9e35831ab128e20903d8c4
Rhyn <rhyyn.lol@gmail.com>
2022-05-22 11:31:38 +0200

    Added temporary project list

--- pages/projects.jsx
+++ pages/projects.jsx
@@ -1,11 +1,33 @@
 import React from "react";
 import homeStyles from "../styles/Home.module.css";
 import Header from "../components/Header";
+import Card from "../components/Card";
 
 const projects = () => {
+  const projectsList = {
+    projects: [
+      {
+        title: "Portfolio",
+        desc: "Portfolio I built with NextJs, contains my Skills, my Projects, a little bit About Me and a Contact form",
+        image: "/portfolio01.png",
+      },
+      {
+        title: "Lost Ark Wandering Merchant Tracker",
+        desc: "Website used to track Merchants spawns in the game Lost Ark, available in US/EU/Asia, Possibility of activating sound reminders",
+      },
+      {
+        title: "Harry Potter character displayer",
+        desc: "Website that pulls data from the Harry Potter API and display characters cards, Search and filter by Students/Houses/Teacher and more",
+      },
+    ],
+  };
+  
   return (
     <div className={homeStyles.pageContainer}>
       <Header></Header>
+      {projectsList.projects.map((project, index) => {
+        return <Card key={index} project={project} src={project.image}></Card>;
+      })}
     </div>
   );
 };
