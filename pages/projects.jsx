85f8129e053e8f153f406e02e2665845ab8295ec
Rhyn <rhyyn.lol@gmail.com>
2022-05-22 16:43:16 +0200

    Added some more data to projects

--- pages/projects.jsx
+++ pages/projects.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import homeStyles from "../styles/Home.module.css";
+import projectStyles from "../styles/Project.module.css";
 import Header from "../components/Header";
 import Card from "../components/Card";
 
@@ -9,25 +10,36 @@ const projects = () => {
       {
         title: "Portfolio",
         desc: "Portfolio I built with NextJs, contains my Skills, my Projects, a little bit About Me and a Contact form",
-        image: "/portfolio01.png",
+        image: "/portfolio02.png",
       },
       {
         title: "Lost Ark Wandering Merchant Tracker",
         desc: "Website used to track Merchants spawns in the game Lost Ark, available in US/EU/Asia, Possibility of activating sound reminders",
+        image: "/loatracker.png",
       },
       {
         title: "Harry Potter character displayer",
         desc: "Website that pulls data from the Harry Potter API and display characters cards, Search and filter by Students/Houses/Teacher and more",
+        image: "/portfolio02.png",
+      },
+      {
+        title: "Harry Potter character displayer",
+        desc: "Website that pulls data from the Harry Potter API and display characters cards, Search and filter by Students/Houses/Teacher and more",
+        image: "/portfolio02.png",
       },
     ],
   };
-  
+
   return (
     <div className={homeStyles.pageContainer}>
       <Header></Header>
-      {projectsList.projects.map((project, index) => {
-        return <Card key={index} project={project} src={project.image}></Card>;
-      })}
+      <div className={projectStyles.cardBox}>
+        {projectsList.projects.map((project, index) => {
+          return (
+            <Card key={index} project={project}></Card>
+          );
+        })}
+      </div>
     </div>
   );
 };
