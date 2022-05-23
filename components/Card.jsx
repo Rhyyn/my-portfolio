a8e6a04b1fc16c918cae9148eab4d7ed2f6a9ce5
Rhyn <rhyyn.lol@gmail.com>
2022-05-23 15:31:06 +0200

    Card formatting

--- components/Card.jsx
+++ components/Card.jsx
@@ -5,9 +5,15 @@ import projectStyles from "../styles/Project.module.css";
 const Card = (props) => {
   return (
     <div className={projectStyles.card}>
-    <img src={props.project.image} className={projectStyles.image}/>
-      <h1>{props.project.title}</h1>
-      <p>{props.project.desc}</p>
+      <div className = {projectStyles.imageContainer}>
+        <img src={props.project.image} className={projectStyles.image} />
+      </div>
+      <div className = {projectStyles.textContainer}>
+        <h1>{props.project.title}</h1>
+        <p>{props.project.desc}</p>
+        <button>Visit Website</button>
+        <button>View Source Code</button>
+      </div>
     </div>
   );
 };
