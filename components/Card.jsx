379fab3bb86cba42ffdcf40a29abb952cbd284ed
Tony Migeon <rhyyn.lol@gmail.com>
2022-06-02 16:45:58 +0200

    Start of Card component

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,12 +1,34 @@
 import React from "react";
-import projects from "../pages/projects";
-import projectStyles from "../styles/Project.module.css";
+import {
+    Button,
+    Card,
+    CardActions,
+    CardContent,
+    CardHeader,
+    CardMedia,
+    Typography,
+} from "@mui/material";
+import { SiGithub } from "react-icons/si";
 
-const Card = (props) => {
-  return (
-    <div className={projectStyles.card}>
-    </div>
-  );
+const MyCard = (props) => {
+    return (
+        <Card sx={{ maxWidth: "240px", margin: "1rem" }}>
+            <CardHeader title={props.name}></CardHeader>
+            <CardMedia
+                component="img"
+                height="140"
+                image={props.image}
+            ></CardMedia>
+            <CardContent>
+                <Typography variant="subtitle2">{props.desc}</Typography>
+            </CardContent>
+            <SiGithub size={20} />
+            <CardActions>
+                <Button size="small">Live Demo</Button>
+                <Button size="small">Learn More</Button>
+            </CardActions>
+        </Card>
+    );
 };
 
-export default Card;
+export default MyCard;
