1c22fc756d69792851a03b29010762f9dbf285fd
Tony Migeon <rhyyn.lol@gmail.com>
2022-06-03 11:37:33 +0200

    Feat: Live Demo / Github Repo link open in new tab

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 import {
     Button,
     Card,
@@ -12,20 +13,25 @@ import { SiGithub } from "react-icons/si";
 
 const MyCard = (props) => {
     return (
-        <Card sx={{ maxWidth: "300px", margin: "1rem", textAlign: 'center', color: 'primary.main' }}>
+        <Card sx={{ maxWidth: "300px", margin: "1rem", textAlign: 'center', color: 'primary.main', maxHeight: "660px" }}>
             <CardHeader title={props.name} ></CardHeader>
             <CardMedia
                 component="img"
-                height="100%"
+                height="398px"
                 image={props.image}
             ></CardMedia>
             <CardContent >
                 <Typography variant="subtitle2">{props.desc}</Typography>
             </CardContent>
-            <SiGithub size={20} />
+            
+            <Link href={props.repo} >
+                                <a target="_blank">
+                                <SiGithub size={20} color='#cc2b84'/>
+                                </a>
+                            </Link>
             <CardActions>
-                <Button size="small">Live Demo</Button>
-                <Button size="small">Learn More</Button>
+                <Button size="small" sx={{color:"secondary.main"}} href={props.demo} target="_blank">Live Demo</Button>
+                <Button size="small" sx={{color:"secondary.main"}}>Learn More</Button>
             </CardActions>
         </Card>
     );
