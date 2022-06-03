062af42543bfe8f85821c542f20a763c2d36d441
Tony Migeon <rhyyn.lol@gmail.com>
2022-06-03 11:09:04 +0200

    FIx: Typography not applying

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import "../styles/globals.css";
-import { createTheme, colors } from "@mui/material";
+import { createTheme, colors } from "@mui/material/styles";
 import { ThemeProvider } from "@mui/material/styles";
 
 function MyApp({ Component, pageProps }) {
@@ -19,21 +19,21 @@ function MyApp({ Component, pageProps }) {
                 50: "#f0f0f0",
                 100: "#e9e9e9",
             },
-            typography: {
-                fontFamily: "Montserrat",
-                fontWeightLight: "400",
-                fontWeightRegular: "500",
-                fontWeightMedium: "600"
-            },
+        },
+        typography: {
+            fontFamily: "Montserrat",
+            fontWeightLight: "400",
+            fontWeightRegular: "500",
+            fontWeightMedium: "600",
         },
         breakpoints: {
             values: {
                 mobile: 0,
                 tablet: 550,
                 laptop: 1024,
-                desktop: 1200
-            }
-        }
+                desktop: 1200,
+            },
+        },
     });
     return (
         <ThemeProvider theme={theme}>
