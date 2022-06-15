b21826b2ba3d67e8b9c3e32f03671e4fd7fe9831
Tony Migeon <rhyyn.lol@gmail.com>
2022-06-15 13:28:10 +0200

    Added locale content

--- components/HomeProjects.jsx
+++ components/HomeProjects.jsx
@@ -2,25 +2,46 @@ import React from "react";
 import { styled } from "@mui/system";
 import { Typography } from "@mui/material";
 import MyCard from "./Card";
+import { useRouter } from "next/router";
 
 const HomeProjects = () => {
 
+    const Content = {
+        en: {
+            sectionTitle: "My Projects",
+            
+        },
+        fr: {
+            sectionTitle: "Mes Projets",
+            
+        },
+    };
+    const router = useRouter();
+    const { locale, locales, defaultLocale, asPath } = useRouter();
+    const { sectionTitle } =
+        Content[locale];
 
     const Projects = {
         projects: [
             {
-                name: "Harry Potter Character Displayer",
+                englishName: "Harry Potter Character Displayer",
+                frenchName: "Librairie des personnages de Harry Potter",
                 image: "/HpDisplayer.png",
-                description:
-                    "Web App that pulls data from a public API then displays it, search by Name, Houses and more",
+                englishDescription:
+                    "Site fetching data from a public API, then sorting them by characters, user can search by Name, Houses etc..",
                 repo: "https://github.com/Rhyyn/Harry-potter-character-displayer",
+                frenchDescription:
+                    "Site prenant des données d'une API publique, puis les tris par personnages, possibilité de chercher par Nom, Maisons etc..",
                 demo: "http://harry-potter-character-displayer.vercel.app/",
             },
             {
-                name: "Lost Ark Wandering Merchant Timer",
+                englishName: "Lost Ark Wandering Merchant Timer",
+                frenchName: "Lost Ark chronomètre pour Marchants errants",
                 image: "/LostArkTracker.png",
-                description:
-                    "Web App made to track Wandering Merchants timers in the game Lost Ark, available for all Servers",
+                englishDescription:
+                    "Web App made to track Wandering Merchants timers in the game Lost Ark, available for all Servers.",
+                frenchDescription:
+                    "Application faite pour suivre l'apparition de Marchants errants sur le jeu Lost Ark, disponible pour tous les serveurs.",
                 repo: "https://github.com/Rhyyn/Rhyn-Lost-Ark-Wandering-Merchant_Tracker",
                 demo: "https://lostark-merchant-tracker.netlify.app/",
             },
@@ -28,17 +49,19 @@ const HomeProjects = () => {
     };
 
     const HomeProjectsSection = styled("section")(({ theme }) => ({
-        width: "90vw",
+        width: "100vw",
+        maxWidth: "100%",
         margin: "0 auto",
         height: "100%",
-        backgroundColor: "#fcfcfc",  
-        boxShadow:
-            "0px 17px 6px -3px rgba(0,0,0,0.2),0px 17px 14px 1px rgba(0,0,0,0.14),0px 17px 18px 3px rgba(0,0,0,0.12)", // 17px = y
+        backgroundColor: "#fcfcfc",
+        // boxShadow:
+        //     "0px 17px 6px -3px rgba(0,0,0,0.2),0px 17px 14px 1px rgba(0,0,0,0.14),0px 17px 18px 3px rgba(0,0,0,0.12)",
+        // 17px = y
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
         flexDirection: "column",
-        paddingTop: '3rem'
+        paddingTop: "3rem",
     }));
 
     const HomeProjectDiv = styled("div")(({ theme }) => ({
@@ -65,24 +88,15 @@ const HomeProjects = () => {
                     color: "secondary.main",
                     fontWeight: "700",
                     fontSize: "2rem",
-                    paddingBottom: '2rem'
+                    paddingBottom: "2rem",
                 }}
             >
-                My Projects
+                {sectionTitle}
             </Typography>
-            
+
             <HomeProjectDiv>
                 {Projects.projects.map((project, index) => {
-                    return (
-                        <MyCard
-                            key={index}
-                            name={project.name}
-                            image={project.image}
-                            desc={project.description}
-                            repo={project.repo}
-                            demo={project.demo}
-                        ></MyCard>
-                    );
+                    return <MyCard key={index} project={project}></MyCard>;
                 })}
             </HomeProjectDiv>
         </HomeProjectsSection>
